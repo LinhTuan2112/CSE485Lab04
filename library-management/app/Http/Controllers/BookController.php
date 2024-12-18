6639018eec2161ecb8e5986bd265acfae3e3d0a2
Linh <linh@gami.com>
2024-12-18 17:41:45 +0700

    CSDL
    CSDL của các bảng Books, Readers, Borrows

---
+++ library-management/app/Http/Controllers/BookController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Book;
+
+class BookController extends Controller
+{
+    public function index()
+    {
+        $books = Book::all();
+        return view('books.index', compact('books'));
+    }
+
+    public function create()
+    {
+        return view('books.create');
+    }
+
+    public function store(Request $request)
+    {
+        Book::create($request->all());
+        return redirect()->route('books.index');
+    }
+}
